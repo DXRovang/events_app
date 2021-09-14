7270d036a68e3b4f21d90e6b1842222b9b6e89c6
Alexander Rovang <wolf_vii@yahoo.com>
2021-09-14 16:18:12 -0700

    adds index, create, and event_params to EventsController

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,15 @@
 class EventsController < ApplicationController
+  def index
+    events = Event.all 
+    render json: events
+  end
+  def create
+    event = Event.create(event_params)
+    render json: event
+  end
+
+  def event_params
+    params.require("event").permit(:name, :event_type)
+  end
+  
 end
